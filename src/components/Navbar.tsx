a8cf7e5f1a3b85551f7f5b7b841bf614fdcceda4
Maryiamreda <maryiamreda@gmail.com>
2025-03-07 23:11:08 +0200

    navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,19 @@
 
 const Navbar = () => {
     return (
-        <div className="w-full px-28 py-10 flex  justify-between">
-            <img src="/images/logo.svg" />
-            <div className="flex">
-                <p>Home</p>
-                <p>About</p>
-                <p>Contact</p>
-                <p>Blog</p>
-                <p>Careers</p>
-
+        <div className="w-full px-28 pt-3 pb-2  flex  justify-between bg-white">
+            <div className="flex items-center ">
+                <img src="/images/logo.svg" className="w-35 h-6 " />
             </div>
-            <div>Request Invite</div>
+            <ul className="navbar-choices flex gap-7 font-medium  ">
+                <li >Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Blog</li>
+                <li>Careers</li>
+
+            </ul>
+            <div className="navbar-botton text-white px-7 py-3 rounded-3xl text-sm font-medium">Request Invite</div>
         </div>
     );
 }
