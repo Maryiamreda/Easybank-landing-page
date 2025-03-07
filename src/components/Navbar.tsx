aa241649abb11eccb1ac036cff82e15d0d635c35
Maryiamreda <maryiamreda@gmail.com>
2025-03-07 22:12:59 +0200

    navbar html

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,17 @@
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="w-full px-28 py-10 flex  justify-between">
+            <img src="/images/logo.svg" />
+            <div className="flex">
+                <p>Home</p>
+                <p>About</p>
+                <p>Contact</p>
+                <p>Blog</p>
+                <p>Careers</p>
 
+            </div>
+            <div>Request Invite</div>
         </div>
     );
 }
