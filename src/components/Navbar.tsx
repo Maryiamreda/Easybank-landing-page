e1dfed9cb364cac846f8311faf767313324300cc
Maryiamreda <maryiamreda@gmail.com>
2025-03-08 22:07:30 +0200

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
+import { TfiAlignJustify } from "react-icons/tfi";
 
 const Navbar = () => {
     return (
-        <div className="w-full px-28 py-5 flex  justify-between bg-white">
+        <div className="w-full  px-10 lg:px-28 py-5 flex  justify-between items-center bg-white">
             <div className="flex items-center ">
                 <img src="/images/logo.svg" className="w-35 h-6 " />
             </div>
-            <ul className="navbar-choices  font-medium  ">
+            <ul className="navbar-choices  font-medium hidden  md:block ">
                 <li >Home</li>
                 <li>About</li>
                 <li>Contact</li>
@@ -13,7 +14,10 @@ const Navbar = () => {
                 <li>Careers</li>
 
             </ul>
-            <div className="navbar-botton text-white px-7 py-3 rounded-3xl text-sm font-medium cursor-pointer hover:opacity-70">Request Invite</div>
+            <div className="md:hidden">
+                <TfiAlignJustify />
+            </div>
+            <div className="navbar-botton hidden  md:block  text-white  px-7 py-3 rounded-3xl text-sm font-medium cursor-pointer hover:opacity-70">Request Invite</div>
         </div>
     );
 }
