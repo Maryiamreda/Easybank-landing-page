c70a4a5a735780362f0c955de11c5024eb21df08
Maryiamreda <maryiamreda@gmail.com>
2025-03-10 01:33:16 +0200

    some little responsive

--- src/components/Topheader.tsx
+++ src/components/Topheader.tsx
@@ -1,7 +1,7 @@
 
 const Topheader = () => {
     return (
-        <div className="topheader  pl-10 lg:pl-28  flex  overflow-hidden md:h-89 ">
+        <div className="topheader  pl-10 lg:pl-28  flex  md:overflow-hidden md:h-89 flex-col-reverse md:flex-row ">
             <div className="flex flex-col gap-5 justify-center  ">
                 <h1 className="text-3xl font-semibold">  Next generation <br /> digital banking
                 </h1>
@@ -11,8 +11,8 @@ const Topheader = () => {
                 <div className="navbar-botton hidden  md:block  text-white w-34 text-center   py-3 rounded-3xl text-sm font-medium cursor-pointer hover:opacity-70">Request Invite</div>
 
             </div>
-            <div className="relative rotate-10 ">
-                <img src='/images/bg-intro-desktop.svg' className=" absolute -top-60 left-25  max-w-450 w-250 h-200  " />
+            <div className="md:relative rotate-10 ">
+                <img src='/images/bg-intro-desktop.svg' className=" absolute -top-60 lg:-top-70 left-25  max-w-450 w-250 lg:w-500 h-200  " />
 
             </div>
 
